5cc68355a45f55a2e6053b692782f8b19258d47d
Hong Anh Ngo <hatl10603@gmail.com>
2024-11-04 14:57:25 +0700

    Fix filter and sort closing bug.

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// const leftSidebar = document.getElementById("left-sidebar");
-// const rightSidebar = document.getElementById("right-sidebar");
-
-// const closeLeftSidebarBtn = document.getElementById("close-left-sidebar-btn");
-// const closeRightSidebarBtn = document.getElementById("close-right-sidebar-btn");
-
-// const openLeftSidebarBtn = document.getElementById("open-left-sidebar-btn");
-// const openRightSidebarBtn = document.getElementById("open-right-sidebar-btn");
-
-// openLeftSidebarBtn.addEventListener("click", () => {
-//   leftSidebar.style.display = "block";
-// });
-
-// openRightSidebarBtn.addEventListener("click", () => {
-//   rightSidebar.style.display = "block";
-// });
-
 const details = [...document.querySelectorAll("details")];
 document.addEventListener("click", function (e) {
   let isClickInsideDetails = details.some((f) => f.contains(e.target));
